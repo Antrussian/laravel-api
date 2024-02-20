c846d327e9b9678b8164da5d4c5de1443892fdd8
Antrussian <antonino.russo.agent@gmail.com>
2024-02-20 23:03:13 +0100

    creo api e faccio eager loading per passare i dati con i dati relazionati

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,13 +6,38 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
-
 class ProjectController extends Controller
 {
+    /**
+     * Display a listing of the projects.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
-        $projects = Project::all();
+        // Eager load 'technologies' and 'type' relationships
+        $projects = Project::with(['technologies', 'type'])->get();
 
-        return response()->json($projects);
+        return response()->json([
+            "success" => true,
+            "result" => $projects,
+        ]);
+    }
+
+    /**
+     * Display the specified project.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Project $project)
+    {
+        // Eager load 'technologies' and 'type' relationships for the single project
+        $project->load(['technologies', 'type']);
+
+        return response()->json([
+            "success" => true,
+            "result" => $project,
+        ]);
     }
 }
