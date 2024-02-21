dc2b199987a6b013c39f565c91ba71081aee285e
Antrussian <antonino.russo.agent@gmail.com>
2024-02-21 01:25:20 +0100

    Last

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-
+<div></div>
 <div class="container">
     <div class="row">
         <div class="col-12">
@@ -9,7 +9,7 @@
                 Questi sono tutti i progetti disponibili {{ Auth::user()->name }}
             </h2>
 
-            <!-- Pulsante Aggiungi Nuovo Progetto -->
+            <!-- Pulsante Aggiungi  Nuovo Progetto -->
             <a href="{{ route('admin.projects.create') }}" class="btn btn-success mb-3">Aggiungi Nuovo Progetto</a>
 
             <table class="table table-hover">
